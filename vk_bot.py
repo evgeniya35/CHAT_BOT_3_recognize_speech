8fc767b7ffa39784c9998747ea2cbb2f4e423382
evgeniya35 <evgeniya35@yandex.ru>
2022-03-21 20:56:41 +0300

    Add VK bot with DialogFlow

---
+++ vk_bot.py
@@ -0,0 +1,50 @@
+import logging
+import os
+
+from vk_api import VkApi
+from vk_api.longpoll import VkLongPoll, VkEventType
+from vk_api.utils import get_random_id
+
+from environs import Env
+
+from check_intent import detect_intent_texts
+
+env = Env()
+env.read_env()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+def handle_vk_message(event, vk_bot, project_id):
+    intent = detect_intent_texts(
+        project_id=project_id,
+        session_id=event.user_id,
+        text=event.text,
+        language_code='ru-RU'
+    )
+    vk_bot.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        message=intent.query_result.fulfillment_text
+    )
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    vk_token = env.str('VK_APP_TOKEN')
+    project_id = env.str('DF_PROJECT_ID')
+
+    vk_session = VkApi(token=vk_token)
+    longpoll = VkLongPoll(vk_session)
+    vk_bot = vk_session.get_api()
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            handle_vk_message(event, vk_bot, project_id)
+
+
+if __name__ == '__main__':
+    main()
