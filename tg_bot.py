2ee7994abfaf01311cc0cf6b650f317050e4ed02
evgeniya35 <evgeniya35@yandex.ru>
2022-03-26 11:35:05 +0300

    Add log handler

--- tg_bot.py
+++ tg_bot.py
@@ -2,16 +2,16 @@ import logging
 import os
 
 from telegram import Update
+import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from environs import Env
 
 from check_intent import detect_intent_texts
+from tg_log_handler import TelegramLogsHandler
 
 env = Env()
 env.read_env()
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__file__)
 
 
@@ -21,8 +21,7 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Здравствуйте! Чем можем помочь?')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    print(update.message.chat_id)
+def handle_message(update: Update, context: CallbackContext) -> None:
     intent = detect_intent_texts(
         context.bot_data['project_id'],
         update.message.chat_id,
@@ -34,9 +33,17 @@ def echo(update: Update, context: CallbackContext) -> None:
 
 def main():
     tg_token = env.str('TG_TOKEN')
+    tg_token_admin = env.str('TG_TOKEN_ADMIN')
     project_id = env.str('DF_PROJECT_ID')
+    tg_chat_id = env.str('TG_CHAT_ID')
+    
+    tg_adm_bot = telegram.Bot(token=tg_token_admin)
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+        )
+    logger.addHandler(TelegramLogsHandler(tg_adm_bot, tg_chat_id))
     logger.info('TG bot running...')
 
     updater = Updater(tg_token)
@@ -44,7 +51,7 @@ def main():
     dispatcher.bot_data = {'project_id': project_id}
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(MessageHandler(
-        Filters.text & ~Filters.command, echo))
+        Filters.text & ~Filters.command, handle_message))
 
     updater.start_polling()
     updater.idle()
